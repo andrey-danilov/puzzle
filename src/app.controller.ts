2866e4ec2b770501d8cc96532c031a45454510e1
Andrey Danilov <and.re.len.off@gmail.com>
2021-04-16 09:12:26 +0300

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): number[] {
+  getPuzzle(): number[] {
     return this.appService.solvePuzzle();
   }
 }
